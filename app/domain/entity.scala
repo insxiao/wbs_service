d896e070c188eecbf0b463e1a0090f3c27a820b1
Xunxiao Ren <inxiao@outlook.com>
2018-03-05 23:42:22 +0800

    s

--- app/domain/entity.scala
+++ app/domain/entity.scala
@@ -1,14 +1,45 @@
+package repository
+
 import java.util.Date
 
+import repository.Gender.Gender
+import slick.jdbc.PostgresProfile._
+import slick.jdbc.PostgresProfile.api._
+
+import scala.util.Try
+
 object Gender extends Enumeration {
-  val Male, Female = VALUE
+  type Gender = Value
+  val Male, Female = Value
 }
 
 case class User(id: Long,
                 name: String,
+                gender: Gender,
                 birthday: Date,
                 email: String,
                 phone: String)
 
+case class MicroPost(id: Long, content: String, postDate: Date, ownerId: Long)
 
-case class MicroBlog(id: Long, content: String, postDate: Date, ownerId: Long)
+trait UserRepository {
+  def findBy(id: Long): Option[User]
+
+  def findBy(id: Long, password: Array[Char]): Option[User]
+
+  def save(user: User): Try[User]
+
+  def findMyFollowers(user: User): List[User]
+}
+
+trait PostRepository {
+  def findByUserId(userId: Long): List[MicroPost]
+
+  def post(mp: MicroPost): Try[MicroPost]
+}
+
+
+
+
+
+
