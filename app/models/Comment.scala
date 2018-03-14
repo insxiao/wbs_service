5042dce063442ca93ec9039ea7bdbdee6602657d
Xunxiao Ren <inxiao@outlook.com>
2018-03-14 14:58:19 +0800

    add Comment repository

--- app/models/Comment.scala
+++ app/models/Comment.scala
@@ -2,7 +2,7 @@ package models
 
 import java.time.LocalDateTime
 
-case class Comment(id: Option[Long], content: String, stars: Int, userId: Long, timestamp: LocalDateTime)
+case class Comment(id: Option[Long], blogId: Long, content: String, stars: Int, userId: Long, timestamp: LocalDateTime)
 
 object Comment {
   import play.api.libs.json.{Json, Format}
--- app/models/Repository.scala
+++ app/models/Repository.scala
@@ -12,6 +12,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class Repository(private val dbConfigProvider: DatabaseConfigProvider)
                 (implicit val executionContext: ExecutionContext)
   extends RepositoryImplicits {
+  self =>
   type Profile = PostgresProfile
   val dbConfig = dbConfigProvider.get[Profile]
 
@@ -64,9 +65,14 @@ class Repository(private val dbConfigProvider: DatabaseConfigProvider)
     * @param tag
     */
   private class CommentTable(tag: Tag) extends Table[Comment](tag, "COMMENTS") {
-    override def * = (id.?, content, stars, userId, timestamp) <> ((Comment.apply _).tupled, Comment.unapply)
+    override def * = (id.?, blogId, content, stars, userId, timestamp) <> ((Comment.apply _).tupled, Comment.unapply)
 
+    /**
+      * primary key
+      */
     def id = column[Long]("comment_id", O.PrimaryKey, O.AutoInc)
+
+    def blogId = column[Long]("blog_id")
 
     def content = column[String]("content")
 
@@ -78,6 +84,7 @@ class Repository(private val dbConfigProvider: DatabaseConfigProvider)
   }
 
   object Users {
+    val users = self.users
     def create(user: User): Future[User] = create(user.name, user.gender, user.password, user.email, user.birthday)
 
     def create(name: String,
@@ -110,6 +117,7 @@ class Repository(private val dbConfigProvider: DatabaseConfigProvider)
   }
 
   object MicroBlogs {
+    val microBlogs = self.microBlogs
 
     def create(microBlog: MicroBlog): Future[MicroBlog] = db.run {
       (microBlogs.map(mb => (mb.content, mb.timestamp, mb.userId))
@@ -134,6 +142,21 @@ class Repository(private val dbConfigProvider: DatabaseConfigProvider)
   }
 
   object Comments {
+    val comments = self.comments
+    def create(comment: Comment): Future[Comment] = db.run {
+      (comments.map(c => (c.blogId, c.content, c.userId))
+        returning comments.map(c => (c.id, c.stars, c.timestamp))
+        into { case ((blogId, content, userId), (commentId, stars, timestamp)) =>
+        Comment(Some(commentId), blogId, content, stars, userId, timestamp) }
+        ) += (comment.blogId, comment.content, comment.userId)
+    }
 
+    def findByBlogId(id: Long): Future[Seq[Comment]] = db.run {
+      comments.filter(_.id === id).sortBy(_.timestamp.desc).result
+    }
+
+    def delete(id: Long): Future[Int] = db.run {
+      comments.filter(_.id === id).delete
+    }
   }
 }
