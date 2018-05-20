b7d6b2d454b8fcf3f2146979800c82f15916b909
unknown <inxiao@outlook.com>
2018-05-20 22:01:39 +0800

    add token transformer

--- app/controllers/AuthAction.scala
+++ app/controllers/AuthAction.scala
@@ -4,6 +4,7 @@ import models.{Token, User}
 import play.api.Logger
 import play.api.mvc._
 import services.{AuthenticationService, UserService}
+import util.extractors
 import util.extractors.{Base64, BasicAuthorization}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -15,6 +16,8 @@ class AuthRequest[A](val username: String, val password: String, request: Reques
 case class UserRequest[A](user: User, request: Request[A]) extends WrappedRequest[A](request)
 
 case class TokenRequest[A](token: Token, request: Request[A]) extends WrappedRequest[A](request)
+
+case class TokenOptionRequest[A](token: Option[Token], request: Request[A]) extends WrappedRequest[A](request)
 
 class TokenAuthenticate(implicit val executionContext: ExecutionContext)
   extends ActionRefiner[Request, TokenRequest] {
@@ -44,6 +47,22 @@ class TokenAuthenticate(implicit val executionContext: ExecutionContext)
   }
 }
 
+class TokenTransform(implicit val executionContext: ExecutionContext)
+  extends ActionTransformer[Request, TokenOptionRequest] {
+  override protected def transform[A](request: Request[A]): Future[TokenOptionRequest[A]] = Future {
+    request.cookies.get("token")
+      .map(_.value)
+      .map {
+        case Base64(credentials) => credentials
+      }
+      .flatMap {
+        case extractors.Token(id, name) =>
+          Some(TokenOptionRequest(Some(Token(id, name)), request))
+        case _ => None
+      }.getOrElse(TokenOptionRequest(None, request))
+  }
+}
+
 /**
   * 将通过Token验证的用户信息提取出来
   *
@@ -59,6 +78,7 @@ class TokenTransformer(implicit val executionContext: ExecutionContext, private 
 class AuthorizationFilter(implicit val executionContext: ExecutionContext)
   extends ActionRefiner[Request, AuthRequest] {
   private val logger = Logger(classOf[AuthorizationFilter])
+
   override protected def refine[A](request: Request[A]): Future[Either[Result, AuthRequest[A]]] = Future {
     request.headers.get("Authorization") match {
       case Some(BasicAuthorization(username, password)) =>
